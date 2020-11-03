a5e81ff8cc3dd2a3b381ef43dd7dc9782f3a806a
Max Jung <max.jung97@web.de>
2020-11-03 17:33:50 +0100

    initialer commit

---
+++ uebungen/b/08 Dance Floor, Logische Operatoren/dancefloor.js
@@ -0,0 +1,25 @@
+var rgbEnabled = true;
+var randomEnabled = false;
+var blinderEnabled = false;
+var strobeEnabled = false;
+var blackEnabled = false;
+
+function getRandomColor() {
+  return 'red';
+}
+
+function isFieldOn() {
+  return true;
+}
+
+function isStrobeOn() {
+  return false;
+}
+
+function isBlinderOn() {
+  return false;
+}
+
+function isRandomOn() {
+  return false;
+}
